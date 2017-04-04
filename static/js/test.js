28e76ab2402db397a65be0f2352fda01cbb636c6
igribkov <igribkov@corp.badoo.com>
2017-04-04 18:20:41 +0300

    test

--- static/js/test.js
+++ static/js/test.js
@@ -33,6 +33,7 @@ jQuery(document).ready(function() {
             cm_chart.series[0].setData(result['data']['coldwater']);
             cm_chart.series[1].setData(result['data']['hotwater']);
             cm_chart.xAxis[0].setCategories(result['data']['ts']);
+            cm_chart.update();
         } else {
             $('.current_month').html(result['status'] + '<br>' + result['data']);
         }
