50e747b25a67e56a187349e24c906ac38f30b399
igribkov <igribkov@corp.badoo.com>
2017-04-04 18:15:13 +0300

    test

--- static/js/charts.js
+++ static/js/charts.js
@@ -77,9 +77,7 @@ function currentMonthChart()
         },
         legend: {
             labelFormatter: function() {
-                var total = 0;
-                for(var i=this.series.length; i--;) { total += this.series[i]; }
-                return this.name + '- Total: ' + total;
+                return '<div class="' + this.name + '-arrow"></div><span style="font-family: \'Advent Pro\', sans-serif; font-size:16px">' + this.name +'</span><br/><span style="font-size:10px; color:#ababaa">(Total: ' + this.options.total + ')</span>';
             }
         },
         xAxis: {
