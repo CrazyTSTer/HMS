bb93abe7529c66cce8e15ef9fe0f4588db50cbc9
igribkov <igribkov@corp.badoo.com>
2017-04-05 17:10:22 +0300

    test with click

--- static/js/charts.js
+++ static/js/charts.js
@@ -73,7 +73,7 @@ function currentMonthChart()
             type: 'column',
             events:{
                 click:function(e){
-                    alert('x: ' + categories[Math.floor(e.xAxis[0].value)]);
+                    alert('Category: ' + this.category + ', value: ' + this.y);
                 }
             }
         },
