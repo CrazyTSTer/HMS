60e8102f792ee1afb916090c43ab99f8b8ed0e8c
igribkov <igribkov@corp.badoo.com>
2017-04-18 15:59:14 +0300

    create stat for each month

--- WaterStat.php
+++ WaterStat.php
@@ -14,7 +14,7 @@ define('GET_CURRENT_DAY_VALUES',              '(SELECT ts, MAX(coldwater) as col
 define ('GET_CURRENT_MONTH_VALUES_BY_DAYS',   '(SELECT DATE(ts) as ts, MAX(coldwater) as coldwater, MAX(hotwater) as hotwater FROM test
                                               WHERE DATE(ts) < DATE_FORMAT(#date#, \'%Y-%m-01\') GROUP BY (1) ORDER BY ts DESC LIMIT 1)
                                               UNION SELECT DATE(ts) as ts, MAX(coldwater) as coldwater, MAX(hotwater) as hotwater FROM test 
-                                              WHERE DATE(ts) BETWEEN DATE_FORMAT(#date#, \'%Y-%m-01\') AND DATE_FORMAT(#date#, \'%Y-%m-01\') + INTERVAL 1 MONTH GROUP BY (1)');
+                                              WHERE DATE(ts) BETWEEN DATE_FORMAT(#date#, \'%Y-%m-01\') AND (DATE_FORMAT(#date#, \'%Y-%m-01\') + INTERVAL 1 MONTH) - INTERVAL 1 DAY GROUP BY (1)');
 
 define('GET_LAST_12_MONTH_VALUES_BY_MONTHS', 'SELECT DATE_FORMAT(ts, \'%Y-%m\') as ts, MAX(coldwater) as coldwater, MAX(hotwater) as hotwater FROM test
                                               WHERE DATE(ts) BETWEEN (DATE_FORMAT(CURDATE() - INTERVAL 12 MONTH, \'%Y-%m-01\')) AND CURDATE() GROUP BY (1)');
