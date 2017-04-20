3e11c68516823af99dfdbc8133dc8ef0a5ca068f
igribkov <igribkov@corp.badoo.com>
2017-04-20 15:42:47 +0300

    time locale

--- WaterStat.php
+++ WaterStat.php
@@ -44,7 +44,7 @@ class WaterStat
 
     public function init($debug = false)
     {
-        setlocale(LC_ALL, 'ru_RU.UTF-8');
+        setlocale(LC_TIME, 'ru_RU.UTF-8');
         $this->debug = $debug;
         $this->action = Vars::get('action', null);
         if (!$this->action) {
