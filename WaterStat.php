794002d33a4f9a5920a751c238d2f2830c7bd471
igribkov <igribkov@corp.badoo.com>
2017-08-09 19:53:28 +0300

    water meters statistic markup finish

--- WaterStat.php
+++ WaterStat.php
@@ -27,10 +27,10 @@ define('GET_PREV_MONTH_RATE',                 'SELECT MAX(coldwater)-MIN(coldwat
                                                 (SELECT ts, coldwater, hotwater FROM WaterMeter WHERE DATE(ts) = DATE_FORMAT(CURDATE(), \'%Y-%m-01\') - INTERVAL 1 DAY ORDER BY ts DESC LIMIT 1)
                                               ) as smth;');
 
-define('GET_CURRENT_DAY_VALUES',              '(SELECT ts, MAX(coldwater) as coldwater, MAX(hotwater) as hotwater FROM WaterMeter
-                                              WHERE DATE(ts) < DATE(#date#) GROUP BY (1) ORDER BY ts DESC LIMIT 1)
-                                              UNION SELECT ts, coldwater, hotwater FROM WaterMeter WHERE DATE(ts) = DATE(#date#)'
-);
+define('GET_CURRENT_DAY_VALUES',              'SELECT ts, coldwater, hotwater FROM WaterMeter WHERE DATE(ts) = CURDATE() 
+                                                UNION 
+                                                (SELECT ts, coldwater, hotwater FROM WaterMeter WHERE DATE(ts) < CURDATE() ORDER BY ts DESC LIMIT 1) ORDER BY ts');
+
 define('GET_CURRENT_MONTH_VALUES_BY_DAYS',    '(SELECT DATE(ts) as ts, MAX(coldwater) as coldwater, MAX(hotwater) as hotwater FROM WaterMeter
                                               WHERE DATE(ts) = DATE_FORMAT(#date#, \'%Y-%m-01\') - INTERVAL 1 DAY GROUP BY(1))
                                               UNION SELECT DATE(ts) as ts, MAX(coldwater) as coldwater, MAX(hotwater) as hotwater FROM WaterMeter 
