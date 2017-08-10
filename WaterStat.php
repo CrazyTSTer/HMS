250997e9e98c25ee70d1bd99e85d2a32daf19d3e
igribkov <igribkov@corp.badoo.com>
2017-08-10 16:54:40 +0300

    water meters statistic markup finish

--- WaterStat.php
+++ WaterStat.php
@@ -187,7 +187,7 @@ class WaterStat
                 $current_day = $this->db->executeQuery(GET_CURRENT_DAY_VALUES, ['date' => 'CURDATE()']);
                 $current_month = $this->db->executeQuery(GET_CURRENT_MONTH_VALUES_BY_DAYS, ['date' => 'CURDATE()']);
                 $last_12month = $this->db->executeQuery(GET_LAST_12_MONTH_VALUES_BY_MONTHS);
-
+                var_export($last_12month);
                 $ret['current_date'] = date('Y-m-d', $current_ts);
                 $ret['current_values'] = Parser::parserCurrentValues($current_values);
                 $ret['current_day'] = Parser::parseCurrentDay($current_day, $current_ts);
