b7999a100ccc659f933604da2e65760b628ca98b
igribkov <igribkov@corp.badoo.com>
2017-08-17 18:16:07 +0300

    test with new graphs

--- static/js/new_gui.js
+++ static/js/new_gui.js
@@ -125,20 +125,58 @@ function get_main_stats(debug)
     }
 }
 function chart() {
-    setChartGlobalParams();
+    /*setChartGlobalParams();
     currentDayChart();
     currentMonthChart();
-    last12MonthChart();
+    last12MonthChart();*/
 
     executeAjaxRequest({action: 'get', param: 'current'}, function (result) {
         if (result['status'] == 'success') {
-            updateChart(cd_chart, result['data']['current_day']);
+            var ctx = document.getElementById("myChart").getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: result['data']['current_day']['data']['chart_js']['ts'],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
+                    }
+                }
+            });
+            /*updateChart(cd_chart, result['data']['current_day']);
             updateChart(cm_chart, result['data']['current_month'], true);
-            updateChart(last12Month_chart, result['data']['last_12month'], true);
+            updateChart(last12Month_chart, result['data']['last_12month'], true);*/
         } else {
             alert('SMTH GOES WRONG!');
         }
     });
+
 }
 /*function chart() {
     Highcharts.chart('container', {
