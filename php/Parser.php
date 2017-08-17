49d8968c63eaebdefa38dbac0fafa7e695ae82dd
igribkov <igribkov@corp.badoo.com>
2017-08-17 18:46:53 +0300

    test with new graphs

--- php/Parser.php
+++ php/Parser.php
@@ -60,15 +60,6 @@ class Parser
             $ret['data'][self::COLDWATER][] = [$ts, 0];
             $ret['data'][self::HOTWATER][] = [$ts, 0];
 
-            $ret['data']['chart_js'][self::TIMESTAMP][] = date('Y-m-d 00:00:00', strtotime($data[1][self::TIMESTAMP]));
-            $ret['data']['chart_js'][self::COLDWATER][] = 0;
-            $ret['data']['chart_js'][self::HOTWATER][] = 0;
-            for ($i = 1; $i < $data[DB::MYSQL_ROWS_COUNT]; $i++) {
-                $ret['data']['chart_js'][self::TIMESTAMP][] = $data[$i][self::TIMESTAMP];
-                $ret['data']['chart_js'][self::COLDWATER][] = $data[$i][self::COLDWATER] - $coldWaterFirstValue;
-                $ret['data']['chart_js'][self::HOTWATER][] = $data[$i][self::HOTWATER] - $hotWaterFirstValue;
-            }
-
             for ($i = 1; $i < $data[DB::MYSQL_ROWS_COUNT]; $i++) {
                 //Смотрим интервал между двумя точками
                 $current_ts = strtotime($data[$i][self::TIMESTAMP]);
