e229d988b0e9845b2b34489860a987a565a76b7c
igribkov <igribkov@corp.badoo.com>
2017-10-04 15:40:58 +0300

    finish billboard js

--- static/js/charts.js
+++ static/js/charts.js
@@ -2,13 +2,111 @@
  * Created by crazytster on 04.04.17.
  */
 var cd_chart, cm_chart, last12Month_chart;
-var days, months;
-var yAxis = {
-    title: {
-        text: 'Литры (л)'
+var cw_gauge, hw_gauge;
+var dateFormatOptions;
+
+var gaugeOptions = {
+    cube: "",
+    data: {
+        columns: [],
+        type: "gauge",
     },
-    min: 0
+    gauge: {
+        min: 0,
+        max: 1000,
+        label: {
+            format: function(value) {return gaugeOptions.cube + ',' + value;},
+            show: false, //show min max labels
+        },
+        fullCircle: true,
+    },
+    size: {
+        height: ($(window).width() < 768) ? 120 : 150,
+        width: ($(window).width() < 768) ? 120 : 150
+    },
+    tooltip: {
+        show: false
+    },
+    color: {
+        pattern: "",
+    },
+    bindto: ""
 };
+
+var barOptions = {
+    data: {
+        type: "bar",
+        x: "x2",
+        columns: [],
+        colors: {
+            coldwater: "blue",
+            hotwater: "red"
+        }
+    },
+    bar: {
+        width: {
+            ratio: 0.3
+        }
+    },
+    axis: {
+        x: {
+            type: "timeseries",
+            tick: {
+                rotate: 45,
+                format: function (x) {
+                    return x.toLocaleString("ru", dateFormatOptions);
+                }
+            },
+            height: ""
+        }
+    },
+    grid: {
+        y: {
+            show: true
+        },
+    },
+    bindto: ""
+};
+
+$(window).resize(function() {
+    if ($(window).width() < 768) {
+        cw_gauge.resize({
+            height:120,
+            width:120
+        });
+        hw_gauge.resize({
+            height:120,
+            width:120
+        });
+    } else {
+        cw_gauge.resize({
+            height:150,
+            width:150
+        });
+        hw_gauge.resize({
+            height:150,
+            width:150
+        });
+    }
+});
+
+function generateGauge(key, value)
+{
+    gaugeOptions.data.columns = [["data", value['liter']]];
+    gaugeOptions.cube = value['cube'];
+
+    if (key == 'coldwater') {
+        gaugeOptions.bindto = "#coldwater";
+        gaugeOptions.color.pattern = ["blue"];
+        $("#coldwater").html("");
+        cw_gauge = bb.generate(gaugeOptions);
+    } else if (key == 'hotwater') {
+        gaugeOptions.bindto = "#hotwater";
+        gaugeOptions.color.pattern = ["red"];
+        $("#hotwater").html("");
+        hw_gauge = bb.generate(gaugeOptions);
+    }
+}
 
 function generateChart(key, value)
 {
@@ -56,348 +154,37 @@ function generateChart(key, value)
                     show: true
                 }
             },
-            /*zoom: {
-                enabled: true
-            },*/
         });
     }
     if (key == 'current_month' && value['status'] == 'success') {
-        cm_chart = bb.generate({
-            data: {
-                type: "bar",
-                x: "x2",
-                columns: [
-                    value['data']['bb']['tsx2'],
-                    value['data']['bb']['coldwater'],
-                    value['data']['bb']['hotwater'],
-                ],
-                colors: {
-                    coldwater: "blue",
-                    hotwater: "red"
-                }
-            },
-            bar: {
-                width: {
-                    ratio: 0.5
-                }
-            },
-            axis: {
-                x: {
-                    type: "timeseries",
-                    tick: {
-                        rotate: 75,
-                        format: function (x) {
-                            var options = {
-                                month: 'short',
-                                day: 'numeric',
-                                weekday: 'short',
-                            };
-                            return x.toLocaleString("ru", options);
-                        }
-                    },
-                    height: 50
-                }
-            },
-            bindto: "#cm_chart"
-        });
+        dateFormatOptions = {
+            month: 'short',
+            day: 'numeric',
+            weekday: 'short',
+        };
+
+        barOptions.data.columns = [
+            value['data']['bb']['tsx2'],
+            value['data']['bb']['coldwater'],
+            value['data']['bb']['hotwater']
+        ];
+        barOptions.axis.x.height = 80;
+        barOptions.bindto = "#cm_chart"
+        cm_chart = bb.generate(barOptions);
     }
     if (key == 'last_12month' && value['status'] == 'success') {
-        last12Month_chart = bb.generate({
-            data: {
-                type: "bar",
-                x: "x2",
-                columns: [
-                    value['data']['bb']['tsx2'],
-                    value['data']['bb']['coldwater'],
-                    value['data']['bb']['hotwater'],
-                ],
-                colors: {
-                    coldwater: "blue",
-                    hotwater: "red"
-                }
-            },
-            bar: {
-                width: {
-                    ratio: 0.5
-                }
-            },
-            axis: {
-                x: {
-                    type: "timeseries",
-                    tick: {
-                        rotate: 75,
-                        format: function (x) {
-                            var options = {
-                                year: 'numeric',
-                                month: 'short',
-                            };
-                            return x.toLocaleString("ru", options);
-                        }
-                    },
-                    height: 80
-                }
-            },
-            bindto: "#last12Month_chart"
-        });
+        dateFormatOptions = {
+            year: 'numeric',
+            month: 'short',
+        };
+
+        barOptions.data.columns = [
+            value['data']['bb']['tsx2'],
+            value['data']['bb']['coldwater'],
+            value['data']['bb']['hotwater']
+        ];
+        barOptions.axis.x.height = 80;
+        barOptions.bindto = "#last12Month_chart"
+        last12Month_chart = bb.generate(barOptions);
     }
-}
-
-function setChartGlobalParams()
-{
-    Highcharts.setOptions({
-        global: {
-            useUTC: false
-        },
-        lang: {
-            loading: 'Загрузка...',
-            months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-            weekdays: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-            shortMonths: ['Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сент', 'Окт', 'Нояб', 'Дек'],
-            exportButtonTitle: "Экспорт",
-            printButtonTitle: "Печать",
-            rangeSelectorFrom: "С",
-            rangeSelectorTo: "По",
-            rangeSelectorZoom: "Период",
-            downloadPNG: 'Скачать PNG',
-            downloadJPEG: 'Скачать JPEG',
-            downloadPDF: 'Скачать PDF',
-            downloadSVG: 'Скачать SVG',
-            printChart: 'Напечатать график'
-        }
-    });
-}
-
-function addSeries(chart)
-{
-    chart.addSeries({
-        name: 'Холодная вода',
-        color: '#7cb5ec',
-        data: []
-    }, false);
-    chart.addSeries({
-        name: 'Горячая вода',
-        color: '#f45b5b',
-        data: []
-    }, false);
-    chart.redraw();
-}
-
-function selectSeries(chart)
-{
-    chart.series[0].data.forEach(function(e){
-        e.update({ color: '#7cb5ec' }, true, false);
-    });
-    chart.series[1].data.forEach(function(e){
-        e.update({ color: '#f45b5b' }, true, false);
-    });
-    chart.series[0].data[chart.columnIndex].update({ color: 'blue' }, true, false);
-    chart.series[1].data[chart.columnIndex].update({ color: 'red' }, true, false);
-    chart.redraw();
-
-    if (chart.name == 'cm_chart') {
-        var day = days[chart.columnIndex];
-        executeAjaxRequest({action: 'get', param: 'day', date: day}, function (result) {
-            if (result['data']['current_day']['status'] = 'success') {
-                updateChart(cd_chart, result['data']['current_day']);
-            } else {
-                $('.current_day').html(result['data']['current_day']['status'] + '<br>' + result['data']['current_day']['data']);
-            }
-        });
-    }
-
-    if (chart.name == 'last12Month_chart') {
-        var month = months[chart.columnIndex];
-        executeAjaxRequest({action: 'get', param: 'month', date: month}, function (result) {
-            if (result['data']['current_month']['status'] = 'success') {
-                days = result['data']['current_month']['data']['ts'][1];
-                updateChart(cm_chart, result['data']['current_month']);
-            } else {
-                $('.current_month').html(result['data']['current_month']['status'] + '<br>' + result['data']['current_month']['data']);
-            }
-        });
-    }
-}
-
-function updateChart(chart, data, selectSeries)
-{
-    if (data['status'] == 'success') {
-        selectSeries = selectSeries || false;
-        if (chart.name == 'cm_chart') {
-            chart.xAxis[0].setCategories(data['data']['ts'][0]);
-            days = data['data']['ts'][1];
-        }
-        if (chart.name == 'last12Month_chart') {
-            chart.xAxis[0].setCategories(data['data']['ts'][0]);
-            months = data['data']['ts'][1];
-        }
-        if (chart.name == 'cd_chart') {
-            chart.setTitle(null, {text: data['data']['date']});
-        }
-        chart.series[0].setData(data['data']['coldwater']);
-        chart.series[1].setData(data['data']['hotwater']);
-        if (selectSeries) {
-            var col_count = data['data']['ts'][0].length - 1;
-            chart.series[0].data[col_count].color = "blue";
-            chart.series[1].data[col_count].color = "red";
-        }
-        chart.redraw();
-        chart.legend.update();
-    } else {
-        $('.' + chart.name).html(data['status'] + '<br>' + data['data']);
-    }
-
-}
-
-function tooltipFormatter(obj, chart, tooltip)
-{
-    var items = obj.points || splat(obj), s;
-    chart.columnIndex = chart.options.xAxis[0].categories.indexOf(obj.x);
-    // Build the header
-    s = [tooltip.tooltipFooterHeaderFormatter(items[0])];
-    // build the values
-    s = s.concat(tooltip.bodyFormatter(items));
-    // footer
-    s.push(tooltip.tooltipFooterHeaderFormatter(items[0], true));
-    return s;
-}
-
-function currentDayChart()
-{
-    cd_chart = Highcharts.chart('cd_chart', {
-        chart: {
-            type: 'spline',
-            zoomType: 'x'
-        },
-        title: {
-            text: 'Потребление холодной и горячей воды за день'
-        },
-        subtitle: {
-            text: ''
-        },
-        xAxis: {
-            type: 'datetime',
-            dateTimeLabelFormats: {
-                hour: '%H:%M'
-            },
-            title: {
-                text: 'Время (ЧЧ:ММ)'
-            }
-        },
-        yAxis: yAxis,
-        legend: {
-            labelFormatter: function() {
-                return this.name + ': ' + this.yData[this.yData.length - 1];
-            }
-        },
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br>',
-            pointFormat: '{point.x:%H:%M:%S}: {point.y:2f} л'
-        },
-        plotOptions: {
-            spline: {
-                marker: {
-                    enabled: true
-                }
-            }
-        }
-    });
-    addSeries(cd_chart);
-    cd_chart.name = 'cd_chart';
-}
-
-function currentMonthChart()
-{
-    cm_chart = Highcharts.chart('cm_chart', {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Потребление холодной и горячей воды за месяц'
-        },
-        subtitle: {
-            text: '(разбивка по дням)'
-        },
-        xAxis: {
-            title: {
-                text: 'Число'
-            },
-            categories: [],
-            crosshair: {
-                enabled: true,
-                events: {click: function() {selectSeries(cm_chart);}}
-            }
-        },
-        yAxis: yAxis,
-        legend: {
-            labelFormatter: function() {
-                var total = 0;
-                for(var i=this.yData.length; i--;) { total += this.yData[i]; };
-                return this.name + ' - Всего: ' + total;
-            }
-        },
-        tooltip: {
-            //headerFormat: '<span style="font-size:14px"><b>{point.key}</b></span><table>',
-            //pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td><td style="padding:0"><b>{point.y:1f} л</b></td></tr>',
-            //footerFormat: '</table>',
-            shared: true,
-            //useHTML: true,
-            formatter: function(tooltip) {return tooltipFormatter(this, cm_chart, tooltip);}
-        },
-        plotOptions: {
-            series: {
-                events: {click: function() {selectSeries(cm_chart);}}
-            }
-        }
-    });
-    addSeries(cm_chart);
-    cm_chart.name = 'cm_chart';
-}
-
-function last12MonthChart()
-{
-    last12Month_chart = Highcharts.chart('last12Month_chart', {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Потребление холодной и горячей воды за последние 12 месяцев'
-        },
-        subtitle: {
-            text: '(разбивка по месяцам)'
-        },
-        xAxis: {
-            title: {
-                text: 'Число'
-            },
-            categories: [],
-            crosshair: {
-                enabled: true,
-                events: {click: function() {selectSeries(last12Month_chart);}}
-            }
-        },
-        yAxis: yAxis,
-        legend: {
-            labelFormatter: function() {
-                var total = 0;
-                var length = this.yData.length;
-                for(var i = 0; i < length; i++) {total += this.yData[i];}
-                return '<b>' + this.name + ':</b>' + '<br>- Всего: ' + total + '<br>- Среднее за месяц: ' + (+(total/length).toFixed(2));
-            }
-        },
-        tooltip: {
-            //headerFormat: '<span style="font-size:14px"><b>{point.key}</b></span><table>',
-            //pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td><td style="padding:0"><b>{point.y:1f} л</b></td></tr>',
-            //footerFormat: '</table>',
-            shared: true,
-            //useHTML: true,
-            formatter: function(tooltip) {return tooltipFormatter(this, last12Month_chart, tooltip);}
-        },
-        plotOptions: {
-            series: {
-                events: {click: function() {selectSeries(last12Month_chart);}}
-            }
-        }
-    });
-    addSeries(last12Month_chart);
-    last12Month_chart.name = 'last12Month_chart';
 }
--- static/js/new_gui.js
+++ static/js/new_gui.js
@@ -7,47 +7,8 @@
  $(this).collapse('hide');
  }
  });*/
-
-var chart_common = {
-    textVertPosition: 0.8,
-    waveAnimateTime: 5000,
-    waveHeight: 0.15,
-    waveAnimate: true,
-    waveOffset: 0.25,
-    valueCountUp: true,
-    displayPercent: false,
-    maxValue: 1000,
-    textSize: 0.5,
-};
-
-var chart, chart1;
-
-$(window).resize(function() {
-    if ($(window).width() < 768) {
-        chart.resize({
-            height:125,
-            width:125
-        });
-        chart1.resize({
-            height:125,
-            width:125
-        });
-    } else {
-        chart.resize({
-            height:150,
-            width:150
-        });
-        chart1.resize({
-            height:150,
-            width:150
-        });
-    }
-});
-
-
 jQuery(document).ready(function() {
     $(".js_set_focus").focus();
-
     show_main_stats();
 });
 
@@ -56,92 +17,26 @@ function show_main_stats()
     $('.js_water_graphs').hide();
     $('.js_main_stats').show();
     executeAjaxRequest({action: 'get', param: 'current_val'}, function (result) {
-        var last_insert = result['data']['ts'];
+        if (result['status'] == 'success') {
+            $(".js_last_insert").text(result['data']['ts']);
 
-        var cw_cube = result['data']['coldwater']['cube'];
-        var cw_liter = result['data']['coldwater']['liter'];
-        var cw_day_rate = result['data']['coldwater']['day_rate'];
-        var cw_month_rate = result['data']['coldwater']['month_rate'];
-        var cw_prev_month_rate = result['data']['coldwater']['prev_month_rate'];
+            $(".js_cold_current_value").text(result['data']['coldwater']['cube'] + ',' + result['data']['coldwater']['liter']);
+            $(".js_cold_today_rate").text(result['data']['coldwater']['day_rate']);
+            $(".js_cold_month_rate").text(result['data']['coldwater']['month_rate']);
+            $(".js_cold_prev_month_rate").text(result['data']['coldwater']['prev_month_rate']);
 
-        var hw_cube = result['data']['hotwater']['cube'];
-        var hw_liter = result['data']['hotwater']['liter'];
-        var hw_day_rate = result['data']['hotwater']['day_rate'];
-        var hw_month_rate = result['data']['hotwater']['month_rate'];
-        var hw_prev_month_rate = result['data']['hotwater']['prev_month_rate'];
+            $(".js_hot_current_value").text(result['data']['hotwater']['cube'] + ',' + result['data']['hotwater']['liter']);
+            $(".js_hot_today_rate").text(result['data']['hotwater']['day_rate']);
+            $(".js_hot_month_rate").text(result['data']['hotwater']['month_rate']);
+            $(".js_hot_prev_month_rate").text(result['data']['hotwater']['prev_month_rate']);
 
-        $("#coldwater").html("");
-        $("#hotwater").html("");
-
-         chart = bb.generate({
-            data: {
-                columns: [
-                    ["data", cw_liter]
-                ],
-                type: "gauge",
-            },
-            color: {
-                pattern: ["blue"],
-            },
-            gauge: {
-                min: 0,
-                max: 1000,
-                label: {
-                    format: function(value) {return cw_cube + ',' + value;},
-                    show: false, //show min max labels
-                },
-                fullCircle: true,
-            },
-            size: {
-                height: ($(window).width() < 768) ? 125 : 150,
-                width: ($(window).width() < 768) ? 125 : 150
-            },
-            tooltip: {
-                show: false
-            },
-            bindto: "#coldwater"
-        });
-
-         chart1 = bb.generate({
-            data: {
-                columns: [
-                    ["data", hw_liter]
-                ],
-                type: "gauge",
-            },
-            color: {
-                pattern: ["red"],
-            },
-            gauge: {
-                min: 0,
-                max: 1000,
-                label: {
-                    format: function(value) {return hw_cube + ',' + value;},
-                    show: false, //show min max labels
-                },
-                fullCircle: true,
-            },
-            size: {
-                height: ($(window).width() < 768) ? 125 : 150,
-                width: ($(window).width() < 768) ? 125 : 150
-            },
-            tooltip: {
-                show: false
-            },
-            bindto: "#hotwater"
-        });
-
-        $(".js_last_insert").text(last_insert);
-
-        $(".js_cold_current_value").text(cw_cube + ',' + cw_liter);
-        $(".js_cold_today_rate").text(cw_day_rate);
-        $(".js_cold_month_rate").text(cw_month_rate);
-        $(".js_cold_prev_month_rate").text(cw_prev_month_rate);
-
-        $(".js_hot_current_value").text(hw_cube + ',' + hw_liter);
-        $(".js_hot_today_rate").text(hw_day_rate);
-        $(".js_hot_month_rate").text(hw_month_rate);
-        $(".js_hot_prev_month_rate").text(hw_prev_month_rate);
+            $.each(result['data'], function (key, value) {
+                if (key == 'ts') return;
+                generateGauge(key, value);
+            });
+        } else {
+            alert('SMTH GOES WRONG!');
+        }
     });
 }
 
