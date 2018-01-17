554331ee97dcfa317e49e1c7ae4dd3f1118fab1e
igribkov <igribkov@corp.badoo.com>
2018-01-17 19:37:58 +0300

    test

--- WaterStat.php
+++ WaterStat.php
@@ -4,6 +4,14 @@ ini_set('display_errors', "1");
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 include_once "php/Utils.php";
+
+/*CREATE TABLE `Water` (
+`ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `coldwater` int(11) NOT NULL,
+  `hotwater` int(11) NOT NULL,
+  PRIMARY KEY (`ts`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8*/
+
 define('CURRENT_DATE',                        'SELECT NOW()');
 define('SET_VALUES',                          'INSERT INTO Water (coldwater, hotwater) VALUES (#coldwater#, #hotwater#)');
 
