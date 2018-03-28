9284e43dd134ad91cd78116901875cd9c0baf1c1
CraztTSTer <crazytster@gmail.com>
2018-03-28 20:10:09 +0300

    experiment

--- static/js/new_gui.js
+++ static/js/new_gui.js
@@ -3,22 +3,27 @@
  */
 
 jQuery(document).ready(function() {
-    /*$(".js_set_focus").focus();
-    show_main_stats('');*/
     $('#sidebar a').on('click', function (e) {
         $('#sidebar').find('.active').removeClass('active');
         if ($(this).attr('data-toggle') != 'collapse') {
             $(this).addClass('active');
         } else {
+            if (!$(this).hasClass('a_sub-active')) {
+                $(this).addClass('a_sub-active');
+            } else {
+                $(this).removeClass('a_sub-active');
+            }
             if ($(this).siblings('ul').hasClass('sub-active')) {
-                var sibl = $(this).siblings('ul');
-                $(sibl).removeClass('sub-active');
-                var fnd = $(sibl).find('.sub-active');
-                var a_sibl = $(fnd).siblings('a').get().reverse();
-                $(a_sibl).click();
+                var el = $(this).siblings('ul').removeClass('sub-active').find('.sub-active').siblings('a').get().reverse();
+                $(el).click();
             } else {
                 $(this).siblings('ul').addClass('sub-active');
             }
         }
+    });
+
+    $('.navbar-toggler').button().on('click', function() {
+        var el = $("#sidebar a[data-toggle*='collapse'][aria-expanded*='true']").get().reverse();
+        $(el).click();
     });
 });
