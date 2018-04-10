b40175d1df1b245437ab5164d5022b2cfeb51454
CraztTSTer <crazytster@gmail.com>
2018-04-10 20:07:57 +0300

    test

--- static/js/new_gui.js
+++ static/js/new_gui.js
@@ -4,21 +4,26 @@
 
 jQuery(document).ready(function() {
     $('#sidebar a').on('click', function (e) {
-
         if ($(this).attr('data-toggle') != 'collapse') {
             $('#sidebar').find('.active').removeClass('active');
             $(this).addClass('active');
+            if ($(this)[0].hasAttribute("data-target")) {
+                $('#content').find('.active').removeClass('active');
+                $($(this).attr('data-target')).addClass('active');
+            }
         } else {
-            /*if (!$(this).hasClass('a_sub-active')) {
+            if (!$(this).hasClass('a_sub-active')) {
                 $(this).addClass('a_sub-active');
             } else {
                 $(this).removeClass('a_sub-active');
-            }*/
+            }
             if ($(this).siblings('ul').hasClass('sub-active')) {
+                //$(this).removeClass('a_sub-active');
                 var el = $(this).siblings('ul').removeClass('sub-active').find('.sub-active').siblings('a').get().reverse();
                 $(el).click();
             } else {
                 $(this).siblings('ul').addClass('sub-active');
+                //$(this).addClass('a_sub-active');
             }
         }
     });
