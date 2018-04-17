db5f3fae5dcfbc2a3feafc4706d338946e36af24
CraztTSTer <crazytster@gmail.com>
2018-04-17 19:28:35 +0300

    test

--- static/js/new_gui.js
+++ static/js/new_gui.js
@@ -59,3 +59,32 @@ function show_main_stats()
         }
     });
 }
+
+function show_graph_rate()
+{
+    var options = {
+        grid: {
+            borderColor: "#3e4e56",
+            borderWidth: 1,
+            backgroundColor: { colors: ["#263238", "#3e4e56"] }
+        },
+
+        yaxis: {
+            tickColor: "#3e4e56", // or same color as background
+        },
+        xaxis: {
+            tickColor: "#3e4e56", // or same color as background
+        }
+    };
+    var d1 = [];
+    for (var i = 0; i < 14; i += 0.5) {
+        d1.push([i, Math.sin(i)]);
+    }
+
+    var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+    var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+
+    $.plot("#day_rate", [ d1, d2, d3 ], options);
+    $.plot("#month_rate", [ d1, d2, d3 ], options);
+    $.plot("#12_month_rate", [ d1, d2, d3 ], options);
+}
