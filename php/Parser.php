27e813748e277a4c1c9af2002daca3bf88b2c1cd
CraztTSTer <crazytster@gmail.com>
2018-05-07 18:09:53 +0300

    test

--- php/Parser.php
+++ php/Parser.php
@@ -36,21 +36,35 @@ class Parser
 
     public static function parseCurrentDay($data)
     {
-        if ($data == false) {
+        if ($data == false || $data == DB::MYSQL_EMPTY_SELECTION) {
             $ret = [
                 "status" => Utils::STATUS_FAIL,
                 "data" => 'Can\'t get current day data from DB'
             ];
-        } elseif ((isset($data[DB::MYSQL_ROWS_COUNT]) && $data[DB::MYSQL_ROWS_COUNT] < 2) || $data == DB::MYSQL_EMPTY_SELECTION) {
-            $ret = [
-                "status" => Utils::STATUS_SUCCESS,
-                "data" => self::EMPTY_DATA
-            ];
+        } elseif (isset($data[DB::MYSQL_ROWS_COUNT]) && $data[DB::MYSQL_ROWS_COUNT] == 1)  {
+            //На начало дня данных нет, поэтому расход нулевой
+            $ret['data'][self::TIMESTAMP . 'cw'][] = 'tscw';
+            $ret['data'][self::TIMESTAMP . 'cw'][] = date("Y-m-d 00:00:00");
+            $ret['data'][self::COLDWATER][] = 'coldwater';
+            $ret['data'][self::COLDWATER][] = 0;
+
+            $ret['data'][self::TIMESTAMP . 'hw'][] = 'tshw';
+            $ret['data'][self::TIMESTAMP . 'hw'][] = date("Y-m-d 00:00:00");
+            $ret['data'][self::HOTWATER][] = 'hotwater';
+            $ret['data'][self::HOTWATER][] = 0;
+
+            //расход по прежнему нулевой до текущего времени, поэтому ресуем точку с текущием временем и нулевым расходом
+            $ret['data'][self::TIMESTAMP . 'cw'][] = date("Y-m-d H:i:s");
+            $ret['data'][self::COLDWATER][] = 0;
+
+            $ret['data'][self::TIMESTAMP . 'hw'][] = date("Y-m-d H:i:s");
+            $ret['data'][self::HOTWATER][] = 0;
+
+            $ret['status'] = Utils::STATUS_SUCCESS;
         } else {
             $coldWaterFirstValue = $data[0][self::COLDWATER];
             $hotWaterFirstValue = $data[0][self::HOTWATER];
             $data[0][self::TIMESTAMP] = date('Y-m-d 00:00:00', strtotime($data[1][self::TIMESTAMP]));
-
 
             //Добавляем дату, которую будем показывать
             $ret['data']['date'] = date('d-m-Y', strtotime($data[1][self::TIMESTAMP]));
@@ -66,9 +80,6 @@ class Parser
             $ret['data'][self::HOTWATER][] = 'hotwater';
             $ret['data'][self::HOTWATER][] = 0;
 
-            $ret['data']['flot'][self::COLDWATER][] = [strtotime($data[0][self::TIMESTAMP]) * 1000, 0];
-            $ret['data']['flot'][self::HOTWATER][] = [strtotime($data[0][self::TIMESTAMP]) * 1000 ,0];
-
             for ($i = 1; $i < $data[DB::MYSQL_ROWS_COUNT]; $i++) {
                 //Смотрим интервал между двумя точками
                 $current_ts = strtotime($data[$i][self::TIMESTAMP]);
@@ -81,14 +92,10 @@ class Parser
                     if ($data[$i][self::COLDWATER] - $data[$i - 1][self::COLDWATER] != 0) {
                         $ret['data'][self::TIMESTAMP . 'cw'][] = date('Y-m-d H:i:s', $point_ts);
                         $ret['data'][self::COLDWATER][] = $data[$i - 1][self::COLDWATER] - $coldWaterFirstValue;
-
-                        $ret['data']['flot'][self::COLDWATER][] = [$point_ts * 1000, $data[$i - 1][self::COLDWATER] - $coldWaterFirstValue];
                     }
                     if ($data[$i][self::HOTWATER] - $data[$i - 1][self::HOTWATER] != 0) {
                         $ret['data'][self::TIMESTAMP . 'hw'][] = date('Y-m-d H:i:s', $point_ts);
                         $ret['data'][self::HOTWATER][] = $data[$i - 1][self::HOTWATER] - $hotWaterFirstValue;
-
-                        $ret['data']['flot'][self::HOTWATER][] = [$point_ts * 1000, $data[$i - 1][self::HOTWATER] - $hotWaterFirstValue];
                     }
                 }
 
@@ -98,10 +105,6 @@ class Parser
 
                 $ret['data'][self::TIMESTAMP . 'hw'][] = $data[$i][self::TIMESTAMP];
                 $ret['data'][self::HOTWATER][] = $data[$i][self::HOTWATER] - $hotWaterFirstValue;
-
-                $ret['data']['flot'][self::COLDWATER][] = [strtotime($data[$i][self::TIMESTAMP]) * 1000, $data[$i][self::COLDWATER] - $coldWaterFirstValue];
-                $ret['data']['flot'][self::HOTWATER][] = [strtotime($data[$i][self::TIMESTAMP]) * 1000, $data[$i][self::HOTWATER] - $hotWaterFirstValue];
-
             }
 
             //Добавляем последнюю точку на вермя текущее время
@@ -110,9 +113,6 @@ class Parser
 
             $ret['data'][self::TIMESTAMP . 'hw'][] = date("Y-m-d H:i:s");
             $ret['data'][self::HOTWATER][] = $data[$data[DB::MYSQL_ROWS_COUNT] - 1][self::HOTWATER] - $hotWaterFirstValue;
-
-            $ret['data']['flot'][self::COLDWATER][] = [strtotime(date("Y-m-d H:i:s")) * 1000, $data[$data[DB::MYSQL_ROWS_COUNT] - 1][self::COLDWATER] - $coldWaterFirstValue];
-            $ret['data']['flot'][self::HOTWATER][] = [strtotime(date("Y-m-d H:i:s")) * 1000, $data[$data[DB::MYSQL_ROWS_COUNT] - 1][self::HOTWATER] - $hotWaterFirstValue];
 
             $ret['status'] = Utils::STATUS_SUCCESS;
         }
