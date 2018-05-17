ed70867c14d8494c5fb1704a83287db49a406586
crazytster <crazytster@gmail.com>
2018-05-17 23:33:27 +0300

    arduino scketch name fix

--- WaterMeter.ino
+++ WaterMeter.ino
@@ -1,6 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h>
+#include "ArduinoJson-v5.13.1.h"
+#include <Ticker.h>
+
 
 #define COLD_PIN D1
 #define HOT_PIN  D2
@@ -9,28 +11,33 @@
 #define ON       LOW
 
 //Константы
-const int MINUTE = 60 * 1000;  //60секунд * 1000милиСекунд
-const int DEBOUNCE_TIME = 100; //Интервал за который должен исчезнуть "дребезг" контактов
-const int SEND_PERIOD = 15;    //Период отправки данных на сервер в минутах
-const int BLINK_PERIOD = 1000; //Интервал мигания диодом при не удачном обращении к серверу
+const int MINUTE             = 60 * 1000; //60секунд * 1000милиСекунд
+const int DEBOUNCE_TIME      = 100;       //Интервал за который должен исчезнуть "дребезг" контактов
+const int SEND_PERIOD        = 1;         //Период отправки данных на сервер в минутах
+const int BLINK_PERIOD       = 1000;      //Интервал мигания диодом при не удачном обращении к серверу
+const int CHECK_METER_PERIOD = 10;         //Интервал опроса счетчика
 
 //Переменные счетчиков
-unsigned int cold_firstTick = 0, cold_lastTick = 0, hot_firstTick = 0, hot_lastTick = 0;
-unsigned int cold_addFirst, cold_addLast, hot_addFirst, hot_addLast;
-unsigned int cold_volume, hot_volume;
-boolean cold_isFirstTick = true, hot_isFirstTick = true;
+int cold_nextPinState, cold_pinState, hot_nextPinState, hot_pinState;
+unsigned int cold_volume, cold_tmpVolume, hot_volume, hot_tmpVolume;
+unsigned long cold_prevMillis, hot_prevMillis;
+
+boolean cold_waitForNextInterrupt = true;
+boolean hot_waitForNextInterrupt = true;
+
+Ticker cold_CheckState, hot_CheckState;
 
 //Переменные работы с контроллером
-unsigned long prev_millis, blink_prev_millis;
-boolean isWiFiConnected = false, sendFailure = false;
+unsigned long prevMillis;
+boolean isWiFiConnected = false;
+Ticker Blink;
 
 //WiFi login and password
 const char* ssid     = "marakaza_2.4";
 const char* password = "pafnutii24";
 
 void setup() {
-	prev_millis = millis();
-	blink_prev_millis = millis();
+	prevMillis = cold_prevMillis = hot_prevMillis = millis();
 
 	pinMode(COLD_PIN, INPUT_PULLUP);
 	pinMode(HOT_PIN, INPUT_PULLUP);
@@ -38,11 +45,10 @@ void setup() {
 	pinMode(LED, OUTPUT);
 	digitalWrite(LED, OFF);
 
-	/*
     //0 -- -- -- -- 2 -- -- -- 9 -- -- -- 0
 	//       2      |    7     |     1
 	//  замкнуто    |разомкнуто| замкнуто
-	//       0           1           0
+	//   0(LOW)       1 (HIGH)    0 (LOW)
 
     //Если контроллер включился когда счетчик находился в положении замкнуто
 	//Тогда с первым изменением состояния счетчика дописываем 3 литра воды (часть уже утекла)
@@ -52,40 +58,39 @@ void setup() {
 
 	//Если контроллер включился когда счетчик находился в положении разомкнуто
 	//Тогда с первым изменением состояния счетчика дописываем 7 литров воды
-	//Должно утечь 3 литра воды*/
+
 	if (digitalRead(COLD_PIN) == LOW) {
-		cold_addFirst = 3;
-		cold_addLast = 7;
+		cold_nextPinState = HIGH;
 	} else {
-		cold_addFirst = 7;
-		cold_addLast = 3;
+		cold_nextPinState = LOW;
 	}
 
 	if (digitalRead(HOT_PIN) == LOW) {
-		hot_addFirst = 3;
-		hot_addLast = 7;
+		hot_nextPinState = HIGH;
 	} else {
-		hot_addFirst = 7;
-		hot_addLast = 3;
+		hot_nextPinState = LOW;
 	}
 
-	attachInterrupt(COLD_PIN, cold_changeState, CHANGE);
-	attachInterrupt(HOT_PIN, hot_changeState, CHANGE);
+	attachInterrupt(COLD_PIN, cold_InterruptHandler, CHANGE);
+	attachInterrupt(HOT_PIN, hot_InterruptHandler, CHANGE);
+
+	cold_CheckState.attach_ms(CHECK_METER_PERIOD, cold_CheckMeterState);
+	hot_CheckState.attach_ms(CHECK_METER_PERIOD, hot_CheckMeterState);
 
 	isWiFiConnected = WiFiConnect();
 }
 
 boolean WiFiConnect()
 {
+	Blink.detach();
 	digitalWrite(LED, OFF);
-	sendFailure = false;
 
 	WiFi.disconnect(true);
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(ssid, password);
 
-	for (int i = 0; i < 60 || WiFi.status() != WL_CONNECTED; i++) {
-		changeLedState();
+	for (int i = 0; i < 60 && WiFi.status() != WL_CONNECTED; i++) {
+		ChangeLedState();
 		delay(500);
 	}
 
@@ -98,37 +103,55 @@ boolean WiFiConnect()
 	return true;
 }
 
-void cold_changeState()
+void cold_InterruptHandler()
 {
-	static unsigned long cold_prevMillis;
-    if(millis() - cold_prevMillis > DEBOUNCE_TIME) {
-        if (cold_isFirstTick) {
-        	cold_firstTick++;
-        	cold_isFirstTick = false;
-        } else {
-        	cold_lastTick++;
-        	cold_isFirstTick = true;
-        }
-    }
     cold_prevMillis = millis();
+    cold_waitForNextInterrupt = false;
 }
 
-void hot_changeState()
+void hot_InterruptHandler()
 {
-	static unsigned long hot_prevMillis;
-    if(millis() - hot_prevMillis > DEBOUNCE_TIME) {
-        if (hot_isFirstTick) {
-        	hot_firstTick++;
-        	hot_isFirstTick = false;
-        } else {
-        	hot_lastTick++;
-        	hot_isFirstTick = true;
-        }
-    }
-    hot_prevMillis = millis();
+	hot_prevMillis = millis();
+	hot_waitForNextInterrupt = false;
 }
 
-boolean sendDataToRemoteHost(int coldwater, int hotwater)
+void cold_CheckMeterState(void)
+{
+	unsigned long tmp;
+	tmp = millis() - cold_prevMillis;
+	if (tmp > DEBOUNCE_TIME && tmp < 3 * DEBOUNCE_TIME && cold_waitForNextInterrupt == false) {
+		cold_pinState = digitalRead(COLD_PIN);
+		if (cold_pinState == cold_nextPinState) {
+			cold_waitForNextInterrupt = true;
+			cold_nextPinState = !cold_nextPinState;
+			if (cold_pinState == LOW) {
+				cold_volume += 7;
+			} else {
+				cold_volume += 3;
+			}
+		}
+	}
+}
+
+void hot_CheckMeterState(void)
+{
+	unsigned long tmp;
+	tmp = millis() - hot_prevMillis;
+	if (tmp > DEBOUNCE_TIME && tmp < 3 * DEBOUNCE_TIME && hot_waitForNextInterrupt == false) {
+		hot_pinState = digitalRead(HOT_PIN);
+		if (hot_pinState == hot_nextPinState) {
+			hot_waitForNextInterrupt = true;
+			hot_nextPinState = !hot_nextPinState;
+			if (hot_pinState == LOW) {
+				hot_volume += 7;
+			} else {
+				hot_volume += 3;
+			}
+		}
+	}
+}
+
+boolean SendDataToRemoteHost(unsigned int coldwater, unsigned int hotwater)
 {
 	if (WiFi.status() != WL_CONNECTED) {
 		return false;
@@ -136,7 +159,7 @@ boolean sendDataToRemoteHost(int coldwater, int hotwater)
 
 	HTTPClient Client;
 
-	Client.begin("http://192.168.1.2/HMS/WaterStat.php?action=set&values[coldwater]=" + String(coldwater) + "&values[hotwater]=" + String(hotwater));
+	Client.begin("http://192.168.1.2/HMS/index.php?action=set&values[coldwater]=" + String(coldwater) + "&values[hotwater]=" + String(hotwater));
 
     int httpCode = Client.GET();
 
@@ -156,7 +179,7 @@ boolean sendDataToRemoteHost(int coldwater, int hotwater)
 	return false;
 }
 
-void changeLedState(void)
+void ChangeLedState(void)
 {
 	if (digitalRead(LED) == OFF) {
 		digitalWrite(LED, ON);
@@ -167,45 +190,33 @@ void changeLedState(void)
 
 void loop()
 {
-	if (cold_firstTick > 0) {
-		cold_firstTick--;
-		cold_volume += cold_addFirst;
-	}
-	if (cold_lastTick > 0) {
-		cold_lastTick--;
-		cold_volume += cold_addLast;
-	}
-
-	if (hot_firstTick > 0) {
-		hot_firstTick--;
-		hot_volume += hot_addFirst;
-	}
-	if (hot_lastTick > 0) {
-		hot_lastTick--;
-		hot_volume += hot_addLast;
-	}
-
-	if (sendFailure == true && millis() - blink_prev_millis > BLINK_PERIOD) {
-		changeLedState();
-		blink_prev_millis = millis();
-	}
-
-	if (millis() - prev_millis > SEND_PERIOD * MINUTE) {
-		prev_millis = millis();
+	if (millis() - prevMillis > SEND_PERIOD * MINUTE) {
 		if (isWiFiConnected && WiFi.status() == WL_CONNECTED) {
 			if (cold_volume != 0 || hot_volume != 0) {
-				boolean result = sendDataToRemoteHost(cold_volume, hot_volume);
+				cold_tmpVolume = cold_volume;
+				hot_tmpVolume = hot_volume;
+				boolean result = SendDataToRemoteHost(cold_tmpVolume, hot_tmpVolume);
 				if (result) {
-					sendFailure = false;
+					if (cold_volume > cold_tmpVolume) {
+						cold_volume -= cold_tmpVolume;
+					} else {
+						cold_volume = 0;
+					}
+					if (hot_volume > hot_tmpVolume) {
+						hot_volume -= hot_tmpVolume;
+					} else {
+						hot_volume = 0;
+					}
+
+					Blink.detach();
 					digitalWrite(LED, ON);
-					cold_volume = 0;
-					hot_volume = 0;
 				} else {
-					sendFailure = true;
+					Blink.attach(1, ChangeLedState);
 				}
 			}
 		} else {
 			isWiFiConnected = WiFiConnect();
 		}
+		prevMillis = millis();
 	}
 }
