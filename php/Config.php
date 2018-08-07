dcc743842374ea102a7559107386a31055b30c83
CraztTSTer <crazytster@gmail.com>
2018-08-07 19:51:17 +0300

    config improvements

--- php/Config.php
+++ php/Config.php
@@ -25,21 +25,12 @@ class Config
         }
         $cfgFullPath = CONFIG_PATH . '/' . $name . '.' . CONFIG_EXT;
         if (file_exists($cfgFullPath)) {
-            $this->config = require($cfgFullPath);
+            $this->config = json_decode(file_get_contents($cfgFullPath), true);
             if (!is_array($this->config)) {
                 throw new Exception('Wrong config contents.');
             }
         }
         $this->name = $name;
-    }
-
-    /**
-     *
-     */
-    public function dump()
-    {
-        var_export($this->config);
-        echo("\n");
     }
 
     /**
@@ -58,36 +49,44 @@ class Config
 
     /**
      * @param $path
-     */
-    public function drop($path)
-    {
-        $path_array = explode('/', $path);
-        $tmp = &$this->config;
-        $last_el = array_pop($path_array);
-        foreach ($path_array as $el) {
-            if (!isset($tmp[$el])) return;
-            $tmp = &$tmp[$el];
-        }
-        unset($tmp[$last_el]);
-    }
-
-    /**
-     * @param $path
      * @return mixed|null
      */
     public function get($path = null)
     {
-        if (!isset($path)) {
-            return $this->config;
-        }
-
-        $path_array = explode('/', $path);
         $tmp = &$this->config;
-        foreach ($path_array as $el) {
-            $tmp = &$tmp[$el] ?? null;
-        }
-
+        if (isset($path)) {
+            $path_array = explode('/', $path);
+            foreach ($path_array as $el) {
+                if (isset($tmp[$el])) {
+                    $tmp = &$tmp[$el];
+                } else {
+                    $tmp = null;
+                }
+            }
+        };
         return $tmp;
+    }
+
+    /**
+     * @param $path
+     */
+    public function drop($path = null)
+    {
+        if (isset($path)) {
+            $path_array = explode('/', $path);
+            $tmp = &$this->config;
+            $last_el = array_pop($path_array);
+            foreach ($path_array as $el) {
+                if (!isset($tmp[$el])) return;
+                $tmp = &$tmp[$el];
+            }
+            if (!isset($tmp[$last_el])) {
+                return;
+            }
+            unset($tmp[$last_el]);
+        } else {
+            $this->config = [];
+        }
     }
 
     /**
@@ -97,7 +96,16 @@ class Config
     {
         file_put_contents(
             CONFIG_PATH . '/' . $this->name . '.' . CONFIG_EXT,
-            '<?php return ' . var_export($this->config, true) . ';'
+            json_encode($this->config, JSON_PRETTY_PRINT)
         );
+    }
+
+    /**
+     *
+     */
+    public function dump()
+    {
+        var_export($this->config);
+        echo("\n");
     }
 }
